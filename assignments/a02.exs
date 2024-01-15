fd383190b082b95b91d700a278a06965cbc3f57a
Iftakhar Husan <AC5636@Labranet.jamk.fi>
2024-01-15 17:30:00 +0200

    Save progress for ex02

--- assignments/a02.exs
+++ assignments/a02.exs
@@ -9,32 +9,109 @@ IO.puts("Number of words in \"#{given_string}\" string is #{word_count}")
 # Part 2
 # -----------------------------------------------------
 given_phrase =
-  "Pattern Matching with Elixir. Remember that equals sign is a match operator, not an assignment"
+  "Pattern xray Matching with Elixir. Remember that equals sign is a match operator, not an assignment"
 
 defmodule PigLatin do
-  @vowels MapSet.new(["a", "e", "i", "o", "u", "yt", "xr"])
-  @min_length_of_vowel Enum.min_by(@vowels, fn v -> String.length(v) end) |> String.length()
-  @max_length_of_vowel Enum.max_by(@vowels, fn v -> String.length(v) end) |> String.length()
-
-  consonants = MapSet.new(["ch", "qu", "squ", "th", "thr", "sch", "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "z"])
-  min_length_of_consonant = Enum.min_by(consonants, fn c -> String.length(c) end) |> String.length()
-  max_length_of_consonant = Enum.max_by(consonants, fn c -> String.length(c) end) |> String.length()
-
   def pig_latinize(phrase) do
     phrase
-      |> String.split(" ")
-      |> Enum.find(fn word -> word end)
-      |> Enum.join(" ")
+    |> String.split(" ")
+    |> Enum.map(fn word ->
+      IO.inspect(classify_word_begining(word))
+    end)
+  end
+
+
+  def pig_latinize_word(word, clasification) do
+
+  end
+
+
+  def classify_word_beginning(word) do
+    consonent = get_starting_consonent_sequence(word)
+    vowel = get_starting_vowel_sequence(word)
+
+    if consonent !== nil do
+      consonent
+    else
+      if vowel !== nil do
+        vowel
+      else
+        {:symbol, String.slice(word, 0, 1)}
+      end
+    end
   end
 
   def get_starting_vowel_sequence(word) do
-    IO.puts("#{@min_length_of_vowel} #{@max_length_of_vowel}")
-    Enum.find_value(Enum.to_list(@min_length_of_vowel..@max_length_of_vowel), fn i ->
-      String.slice(word, 0, i)
-      |> fn v -> if MapSet.member?(vowels, v) do v end end
+    vowels = MapSet.new(["a", "e", "i", "o", "u", "yt", "xr"])
+    min_length_of_vowel = Enum.min_by(vowels, fn v -> String.length(v) end) |> String.length()
+
+    max_length_of_vowel =
+      min(
+        String.length(word),
+        Enum.max_by(vowels, fn v -> String.length(v) end) |> String.length()
+      )
+
+    Enum.find_value(min_length_of_vowel..max_length_of_vowel, fn split_length ->
+      transformed_word = String.downcase(word)
+      candidate_to_check = String.slice(transformed_word, 0, split_length)
+
+      if MapSet.member?(vowels, candidate_to_check) do
+        {:vowel, String.slice(word, 0, split_length)}
+      else
+        nil
+      end
+    end)
+  end
+
+  def get_starting_consonent_sequence(word) do
+    consonants =
+      MapSet.new([
+        "squ",
+        "thr",
+        "sch",
+        "ch",
+        "qu",
+        "th",
+        "b",
+        "c",
+        "d",
+        "f",
+        "g",
+        "h",
+        "j",
+        "k",
+        "l",
+        "m",
+        "n",
+        "p",
+        "q",
+        "r",
+        "s",
+        "t",
+        "v",
+        "w",
+        "x",
+        "z"
+      ])
+
+    min_length_of_consonant =
+      Enum.min_by(consonants, fn c -> String.length(c) end) |> String.length()
+
+    max_length_of_consonant =
+      min(word, Enum.max_by(consonants, fn c -> String.length(c) end) |> String.length())
+
+    Enum.find_value(max_length_of_consonant..min_length_of_consonant//-1, fn split_length ->
+      transformed_word = String.downcase(word)
+      candidate_to_check = String.slice(transformed_word, 0, split_length)
+
+      if MapSet.member?(consonants, candidate_to_check) do
+        {:consonent, String.slice(word, 0, split_length)}
+      else
+        nil
+      end
     end)
   end
 end
 
 
-PigLatin.pig_latinize(given_phrase) |> IO.inspect()
+PigLatin.pig_latinize(given_phrase)
