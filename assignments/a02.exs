b6984696b3d31b86208630a8843eff55bd0245b9
Iftakhar Husan <AC5636@student.jamk.fi>
2024-01-18 00:38:34 +0200

    Solve a02.ex

--- assignments/a02.exs
+++ assignments/a02.exs
@@ -9,36 +9,79 @@ IO.puts("Number of words in \"#{given_string}\" string is #{word_count}")
 # Part 2
 # -----------------------------------------------------
 given_phrase =
-  "Pattern xray Matching with Elixir. Remember that equals sign is a match operator, not an assignment"
+  "Pattern Matching with Elixir. Remember that equals sign is a match operator, not an assignment"
 
 defmodule PigLatin do
+
+  @doc """
+  Given a `phrase`, translates it a word at a time to Pig Latin.
+  * The casing of the original word is preserved.
+  * Ending punctuation is preserved too.
+  """
   def pig_latinize(phrase) do
     phrase
     |> String.split(" ")
-    |> Enum.map(fn word -> pig_latinize_word(word, classify_word_beginning(word)) end)
+    |> Enum.map(&pig_latinize_word/1)
     |> Enum.join(" ")
   end
 
-  def pig_latinize_word(word, clasification) do
-    case clasification do
-      {:vowel, _vowel} -> word <> "ay"
-      {:consonent, consonent} -> String.replace(word, consonent,"") <> consonent <> "ay"
-      _ -> word
-    end
+
+  @doc """
+  Given a `word`, translates it to Pig Latin.
+
+  The function first splits the given word into the word itself and the ending punctuation.
+  In the next step, it classifies the word beginning as a vowel, a consonant, or something else.
+  Then it translates the word according to the classification. Then string concatenation
+  is done with the punctuation. At the last step the original word casing is restored.
+  """
+  def pig_latinize_word(original_word) do
+    {word, ending_punctuation} = split_word_and_ending_punctuation(original_word)
+
+    pig_latin_word =
+      case classify_word_beginning(word) do
+        {:vowel, _} ->
+          word <> "ay" <> ending_punctuation
+
+        {:consonant, consonant} ->
+          word = String.slice(word, String.length(consonant), String.length(word))
+          word <> consonant <> "ay" <> ending_punctuation
+
+        {:other, _} ->
+          word <> ending_punctuation
+      end
+
+    pig_latin_word =
+      if word_capitalized?(original_word) do
+        String.capitalize(pig_latin_word)
+      else
+        pig_latin_word
+      end
+
+    pig_latin_word
+  end
+
+  def word_capitalized?(word) do
+    String.slice(word, 0, 1) == String.slice(word, 0, 1) |> String.upcase()
+  end
+
+  def split_word_and_ending_punctuation(word) do
+    %{
+      "word" => word,
+      "punctuation" => punctuation
+    } = Regex.named_captures(~r/(?<word>.*?)(?<punctuation>[[:punct:]]*$)/, word)
+
+    {word, punctuation}
   end
 
 
   def classify_word_beginning(word) do
-    consonent = get_starting_consonent_sequence(word)
-    vowel = get_starting_vowel_sequence(word)
-
-    if consonent !== nil do
-      consonent
+    if (consonant = get_starting_consonant_sequence(word)) !== nil do
+      consonant
     else
-      if vowel !== nil do
+      if (vowel = get_starting_vowel_sequence(word)) !== nil do
         vowel
       else
-        {:symbol, String.slice(word, 0, 1)}
+        {:other, ""}
       end
     end
   end
@@ -65,7 +108,7 @@ defmodule PigLatin do
     end)
   end
 
-  def get_starting_consonent_sequence(word) do
+  def get_starting_consonant_sequence(word) do
     consonants =
       MapSet.new([
         "squ",
@@ -107,7 +150,7 @@ defmodule PigLatin do
       candidate_to_check = String.slice(transformed_word, 0, split_length)
 
       if MapSet.member?(consonants, candidate_to_check) do
-        {:consonent, String.slice(word, 0, split_length)}
+        {:consonant, String.slice(word, 0, split_length)}
       else
         nil
       end
