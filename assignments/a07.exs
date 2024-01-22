368f9fce6f5c35cf135fbbe200465282d5c479b7
Iftakhar Husan <AC5636@student.jamk.fi>
2024-01-22 13:19:03 +0200

    Setup template for a07

---
+++ assignments/a07.exs
@@ -0,0 +1,53 @@
+defmodule BlackjackDeck do
+  @ranks ~w(2 3 4 5 6 7 8 9 10 Jack Queen King Ace)s
+  @suits ~w(Hearts Diamonds Clubs Spades)s
+
+  def get_deck() do
+    for rank <- @ranks, suit <- @suits, do: {rank, suit}
+  end
+
+  def get_shuffled_deck() do
+    get_deck() |> Enum.shuffle()
+  end
+end
+
+
+defmodule Player do
+  def start() do
+    spawn(__MODULE__, fn state -> loop(state) end, [])
+  end
+
+  def loop(state) do
+    receive do
+
+    end
+  end
+end
+
+defmodule Dealer do
+  def start() do
+    spawn(__MODULE__, fn state -> loop(state) end, [])
+  end
+
+  def loop(state) do
+    receive do
+
+    end
+  end
+end
+
+
+defmodule Blackjack do
+  def start() do
+    spawn(__MODULE__, fn state -> loop(state) end, [])
+  end
+
+  def loop(state) do
+    receive do
+
+    end
+  end
+end
+
+
+IO.inspect(BlackjackDeck.get_shuffled_deck())
