e2a66d2ffde92f539679fcb1f8a6da0f7f64f197
Iftakhar Husan <AC5636@student.jamk.fi>
2024-02-25 21:46:34 +0200

    Solve a09

---
+++ assignments/a09/tcp_client.exs
@@ -0,0 +1,64 @@
+defmodule TCPClient do
+  require Logger
+
+  def start(host \\ ~c"127.0.0.1", port \\4000) when is_integer(port) and port>=1024 and port<=4915 do
+    {:ok, socket} = :gen_tcp.connect(host, port, [:binary, packet: :line, active: false])
+    Logger.info("Connected to #{host}:#{port}")
+    :timer.sleep(100)
+
+    {:ok, data} = :gen_tcp.recv(socket, 0, :timer.seconds(10))
+    case data do
+      "What's your name?\n" ->
+        name = IO.gets("What's your name? ") |> String.trim
+        :gen_tcp.send(socket, "#{name}\n")
+      _ ->
+        Logger.error("Unexpected data: #{inspect(data)}")
+        :gen_tcp.close(socket)
+        Process.exit(self(), :normal)
+    end
+
+    receiver = spawn_link(fn -> server_message_loop(socket) end)
+    _sender = spawn_link(fn -> user_input_loop(socket) end)
+
+    wait_until_process_dies(receiver)
+  end
+
+  defp wait_until_process_dies(pid) do
+    receive do
+    after
+      300 -> if Process.alive?(pid) do
+        wait_until_process_dies(pid)
+      end
+    end
+  end
+
+  defp server_message_loop(socket) do
+    case :gen_tcp.recv(socket, 0) do
+      {:ok, data} ->
+        IO.puts("#{data|>String.trim}")
+        server_message_loop(socket)
+      {:error, :closed} ->
+        Logger.info("Server closed the connection")
+        :ok
+      {:error, reason} ->
+        Logger.error("Error: #{reason}")
+        :ok
+    end
+  end
+
+  defp user_input_loop(socket) do
+    :timer.sleep(100)
+    message = IO.gets("> ") |> String.trim
+
+    if message == "exit" do
+      :gen_tcp.close(socket)
+      Process.exit(self(), :normal)
+    else
+      :gen_tcp.send(socket, "#{message}\n")
+      user_input_loop(socket)
+    end
+  end
+end
+
+
+TCPClient.start()
---
+++ assignments/a09/tcp_server.exs
@@ -0,0 +1,105 @@
+defmodule TCPServer do
+  require Logger
+
+  def start(port \\ 4000) when is_integer(port) and port >= 1024 and port <= 4915 do
+    children = [
+      {Task.Supervisor, name: TCPServer.ClientConnectionSupervisor},
+      Supervisor.child_spec({Task, fn -> accept(port) end}, restart: :permanent)
+    ]
+
+    opts = [strategy: :one_for_one, name: TCPServer.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  defp accept(port) do
+    {:ok, listening_socket} =
+      :gen_tcp.listen(port, [:binary, packet: :line, active: false, reuseaddr: true])
+
+    Logger.info("Listening on port #{port}")
+
+    # Stores names in {socket, name} format
+    :ets.new(:connected_client_names, [:set, :public, :named_table])
+
+    loop_acceptor(listening_socket)
+  end
+
+  defp loop_acceptor(listening_socket) do
+    {:ok, client_socket} = :gen_tcp.accept(listening_socket)
+
+    {:ok, client_connection_process} =
+      Task.Supervisor.start_child(TCPServer.ClientConnectionSupervisor, fn ->
+        # Ask for the client's name and wait for a response for 1 minute
+        :gen_tcp.send(client_socket, "What's your name?\n")
+
+        client_name =
+          case :gen_tcp.recv(client_socket, 0, :timer.minutes(1)) do
+            {:ok, name} ->
+              name |> String.trim()
+
+            {:error, _reason} ->
+              :gen_tcp.close(client_socket)
+              Process.exit(self(), :normal)
+          end
+
+        # Store the client's name and socket in the ETS table to identify the client
+        # this will be used to broadcast messages to all clients by their name
+        :ets.insert_new(:connected_client_names, {client_socket, client_name})
+        Logger.info("#{client_name} connected")
+
+        serve(client_socket, client_name)
+      end)
+
+    :gen_tcp.controlling_process(client_socket, client_connection_process)
+    spawn(fn -> monitor_client_connection(client_connection_process, client_socket) end)
+
+    loop_acceptor(listening_socket)
+  end
+
+  defp monitor_client_connection(pid, socket) do
+    ref = Process.monitor(pid)
+
+    receive do
+      {:DOWN, ^ref, :process, ^pid, _reason} ->
+        :ets.delete(:connected_client_names, socket)
+    end
+  end
+
+  defp serve(client_socket, client_name) do
+    msg = read_line(client_socket)
+
+    case msg do
+      {:ok, data} ->
+        client_sockets = :ets.tab2list(:connected_client_names)
+
+        client_sockets
+        |> Enum.each(fn {socket, name} ->
+          if socket != client_socket do
+            write_line(socket, "#{name}: #{data}")
+          end
+        end)
+
+        serve(client_socket, client_name)
+
+      {:error, _reason} ->
+        Logger.info("Dropped connection with #{client_name}")
+    end
+
+    :gen_tcp.close(client_socket)
+  end
+
+  defp read_line(socket) do
+    :gen_tcp.recv(socket, 0)
+  end
+
+  defp write_line(socket, text) do
+    :gen_tcp.send(socket, text)
+  end
+end
+
+Application.ensure_all_started([:wx, :runtime_tools, :observer])
+:observer.start()
+
+TCPServer.start(4000)
+IO.gets("Press enter to exit\n")
+# Cleanup
+:ets.delete(:connected_client_names)
